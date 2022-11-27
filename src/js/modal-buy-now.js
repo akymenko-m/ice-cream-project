d90a3a34ad4e943ae1a8f4d8f11934227c66d799
ValeriaSarazhinska <lera.sarazhinska@gmail.com>
2022-11-27 19:54:08 +0200

    Refactored js

--- src/js/modal-buy-now.js
+++ src/js/modal-buy-now.js
@@ -1,11 +1,13 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-buy-now-open]"),
+    openModalBtnM: document.querySelector("[data-modal-buy-mobile-open]"),
     closeModalBtn: document.querySelector("[data-modal-buy-now-close]"),
     modal: document.querySelector("[data-modal-buy-now]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtnM.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {

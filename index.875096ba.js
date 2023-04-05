97bda9bdf80ba1cce10bda0c96f91c54bf2ec806
akymenko-m <110409579+akymenko-m@users.noreply.github.com>
2023-04-05 09:11:45 +0000

    Deploying to gh-pages from @ akymenko-m/ice-cream-project@62d6fa953a297ab6018663fcde4dd733bbd9bcad 🚀

---
+++ index.875096ba.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".gallery",{pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});swiper=new Swiper(".review",{pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.875096ba.js.map

3ba9d0f66619c0b8780ea179d42477dcd5225882
akymenko-m <110409579+akymenko-m@users.noreply.github.com>
2023-04-05 16:20:03 +0000

    Deploying to gh-pages from @ akymenko-m/ice-cream-project@8ae90393d32bdac4cc47c2be1cc31e28c94db9e0 🚀

---
+++ index.15d93d7f.js
@@ -0,0 +1,2 @@
+!function(){function e(e,n){var r=document.querySelector(n),t=1,c=setInterval((function(){(t+=1)===e&&clearInterval(c),r.innerHTML=t}),80)}e(16,".header__cafes-number"),e(23,".header__trucks-number")}();
+//# sourceMappingURL=index.15d93d7f.js.map
